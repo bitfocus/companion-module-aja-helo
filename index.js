1aedf123ce169029c81c286025ea984263ab292d
cxselph <cxselph@gmail.com>
2018-07-30 13:09:36 -0400

    updated helo module

--- index.js
+++ index.js
@@ -10,21 +10,9 @@ function instance(system, id, config) {
 
 	self.actions(); // export actions
 
-	// Example: When this script was committed, a fix needed to be made
-	// this will only be run if you had an instance of an older "version" before.
-	// "version" is calculated out from how many upgradescripts your intance config has run.
-	// So just add a addUpgradeScript when you commit a breaking change to the config, that fixes
-	// the config.
-
-	self.addUpgradeScript(function () {
-		// just an example
-		if (self.config.host !== undefined) {
-			self.config.old_host = self.config.host;
-		}
-	});
-
 	return self;
 }
+
 
 instance.prototype.updateConfig = function(config) {
 	var self = this;
@@ -49,7 +37,7 @@ instance.prototype.config_fields = function () {
 			id:    'info',
 			width: 12,
 			label: 'Information',
-			value: 'Recording and streaming profiles are 0-9, not 1-10.'
+			value: 'Recording and streaming profiles are 1-10.'
 		},
 		{
 			type:  'textinput',
@@ -85,8 +73,7 @@ instance.prototype.actions = function(system) {
 				{
 					 type:    'textinput',
 					 label:   'file name',
-					 id:      'idx',
-					 default: 'file_name'
+					 id:      'idx'
 				}
 			]
 		},
@@ -96,9 +83,9 @@ instance.prototype.actions = function(system) {
 			options: [
 				{
 					 type:    'textinput',
-					 label:   'Profile Number 0-9',
+					 label:   'Profile Number 1-10',
 					 id:      'idx',
-					 default: '0'
+					 default: '1'
 				}
 			]
 		},
@@ -118,9 +105,9 @@ instance.prototype.actions = function(system) {
 			options: [
 				{
 					 type:    'textinput',
-					 label:   'Profile Number 0-9',
+					 label:   'Profile Number 1-10',
 					 id:      'idx',
-					 default: '0'
+					 default: '1'
 				}
 			]
 		},
@@ -148,7 +135,7 @@ instance.prototype.action = function(action) {
 			break;
 
 		case 'recordProfile':
-			cmd = 'config?action=set&paramid=eParamID_RecordingProfileSel&value=' + action.options.idx;
+			cmd = 'config?action=set&paramid=eParamID_RecordingProfileSel&value=' + (parseInt(action.options.idx) - 1);
 			break;
 
 		case 'startStream':
@@ -160,15 +147,17 @@ instance.prototype.action = function(action) {
 			break;
 
 		case 'streamProfile':
-			cmd = 'config?action=set&paramid=eParamID_StreamingProfileSel&value=' + action.options.idx;
+			cmd = 'config?action=set&paramid=eParamID_StreamingProfileSel&value=' + (parseInt(action.options.idx) - 1);
 			break;
 
 	}
 
 		if (cmd !== undefined) {
 			self.system.emit('rest_get', 'http://' + self.config.host + '/' + cmd,function (err, data, response) {
-				if (!err) {
-					// Success
+				if (err) {
+					self.log('Error from AJA: ' + response);
+					return;
+					console.log('Result from REST: ', + response);
 				}
 			});
 		}
