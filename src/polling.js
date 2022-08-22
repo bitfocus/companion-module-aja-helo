d825bbb5905630115b7b877b9b2ff25ac0ade19e
caleb-wishart <caleb.wish@outlook.com>
2022-08-22 16:04:36 +1000

    Cleaned up variable naming in polling

--- src/polling.js
+++ src/polling.js
@@ -18,39 +18,39 @@ module.exports = {
 			const connection = new Helo(self.config)
 			self.pollingInterval = setInterval(async () => {
 				// Now get the record status
-				const resultRecord = await connection.sendRequest('action=get&paramid=eParamID_ReplicatorRecordState')
-				self.debug('info', resultRecord)
+				let result = await connection.sendRequest('action=get&paramid=eParamID_ReplicatorRecordState')
+				self.debug('info', result)
 
-				if (resultRecord.status === 'failed') {
+				if (result.status === 'failed') {
 					self.status(self.STATUS_WARNING)
 					return
 				}
 
-				self.STATE.recorder_status_value = resultRecord.response.value
-				self.STATE.recorder_status = resultRecord.response.value_name
+				self.STATE.recorder_status_value = result.response.value
+				self.STATE.recorder_status = result.response.value_name
 
 				// Now get the stream status
-				const resultStream = await connection.sendRequest('action=get&paramid=eParamID_ReplicatorStreamState')
-				self.debug('info', resultStream)
+				result = await connection.sendRequest('action=get&paramid=eParamID_ReplicatorStreamState')
+				self.debug('info', result)
 
-				if (resultStream.status === 'failed') {
+				if (result.status === 'failed') {
 					self.status(self.STATUS_WARNING)
 					return
 				}
 
-				self.STATE.stream_status_value = resultStream.response.value
-				self.STATE.stream_status = resultStream.response.value_name
+				self.STATE.stream_status_value = result.response.value
+				self.STATE.stream_status = result.response.value_name
 
 
-				const resultMedia = await connection.sendRequest('action=get&paramid=eParamID_CurrentMediaAvailable')
-				self.debug('info', resultMedia)
+				result = await connection.sendRequest('action=get&paramid=eParamID_CurrentMediaAvailable')
+				self.debug('info', result)
 
-				if (resultMedia.status === 'failed') {
+				if (result.status === 'failed') {
 					self.status(self.STATUS_WARNING)
 					return
 				}
 
-				self.STATE.storage_media_available = resultMedia.response.value
+				self.STATE.storage_media_available = result.response.value
 
 				self.checkVariables()
 				self.checkFeedbacks()
